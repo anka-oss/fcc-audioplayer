14978edabca439b942fa951544c705d98393eb33
Anna Ossowska <anka.b.bogacka@gmail.com>
2024-03-06 21:19:47 +0100

    Add play functionality

--- script.js
+++ script.js
@@ -91,6 +91,16 @@ const playSong = (id) => {
     audio.src = song.src;
     audio.title = song.title;
 
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData?.songCurrentTime;
+    }
+
+    userData.currentSong = song;
+    
+    playButton.classList.add("playing");
+    audio.play();
 };
 
 const renderSongs = (array) => {
