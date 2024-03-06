e1e721e24d57f944a21490727425190648029adb
Anna Ossowska <anka.b.bogacka@gmail.com>
2024-03-06 21:33:05 +0100

    Add a working 'play button'

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ const playSong = (id) => {
     }
 
     userData.currentSong = song;
-    
+
     playButton.classList.add("playing");
     audio.play();
 };
@@ -108,7 +108,7 @@ const renderSongs = (array) => {
       .map((song)=> {
         return `
             <li id="song-${song.id}" class="playlist-song">
-                <button class="playlist-song-info">
+                <button class="playlist-song-info" onclick="playSong(${song.id})">
                 <span class="playlist-song-title">${song.title}</span>
                 <span class="playlist-song-artist">${song.artist}</span>
                 <span class="playlist-song-duration">${song.duration}</span>
@@ -124,6 +124,14 @@ const renderSongs = (array) => {
 
       playlistSongs.innerHTML = songsHTML;
   };
+
+  playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+      playSong(userData?.songs[0].id);
+    } else {
+      playSong(userData?.currentSong.id);
+    }
+  });
 
   const sortSongs = () => {
     userData?.songs.sort((a,b) => {
