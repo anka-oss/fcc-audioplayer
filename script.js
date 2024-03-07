bcfbbdd7881c78e535a859c04578d68d403f884e
Anna Ossowska <anka.b.bogacka@gmail.com>
2024-03-07 15:44:53 +0100

    Add pause function

--- script.js
+++ script.js
@@ -103,6 +103,13 @@ const playSong = (id) => {
     audio.play();
 };
 
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    
+    playButton.classList.remove("playing");
+    audio.pause();
+};
+
 const renderSongs = (array) => {
     const songsHTML = array
       .map((song)=> {
@@ -132,6 +139,8 @@ const renderSongs = (array) => {
       playSong(userData?.currentSong.id);
     }
   });
+
+  pauseButton.addEventListener("click",  pauseSong);
 
   const sortSongs = () => {
     userData?.songs.sort((a,b) => {
