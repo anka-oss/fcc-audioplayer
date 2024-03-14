fb9be1416f859a351bf0c508288d5c2c3c3ba789
Anna Ossowska <anka.b.bogacka@gmail.com>
2024-03-14 18:00:46 +0100

    Add some settings and visuals for the display

--- script.js
+++ script.js
@@ -132,6 +132,16 @@ const playPreviousSong = () => {
     }
 };
 
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+  
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
     const songToHighlight = document.getElementById(
